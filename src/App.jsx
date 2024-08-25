9e3abde4c1162f7a0edfc7e2e5af2f23fa7316db
saifion33 <saifiazeem33@gmail.com>
2024-08-25 16:18:45 +0530

    fix add story icon

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import InstaHome from './Components/InstaHome'
 function App() {
 
   return (
-    <main className='flex justify-center overflow-hidden w-full h-screen min-h-screen  '>
+    <main className='flex justify-center overflow-hidden w-full lg:h-screen '>
       <section className='max-w-sm w-full relative h-full'>
         <div className='sticky top-0 left-0 w-full max-w-sm mx-auto block'>
           <Header />
--- src/Components/status/Status.jsx
+++ src/Components/status/Status.jsx
@@ -11,7 +11,7 @@ function Status(props) {
         props && props.status && <div className="flex flex-col gap-2 items-center w-fit " >
           <div className="border-2 border-slate-950 rounded-full h-14 w-14  relative">
             <img className="max-w-full  rounded-full" src={props.status.imageUrl} alt="" />
-          {props.status.id == 1 && <FaCirclePlus className='plus absolute -bottom-2 bg-white p-[2px] -right-2 text-blue-600 text-2xl' />}
+          {props.status.id == 1 && <FaCirclePlus className='plus rounded-full absolute -bottom-2 bg-white p-[2px] -right-2 text-blue-600 text-3xl' />}
           </div>
           <p className='name text-sm'>{props.status.name}</p>
         </div>
